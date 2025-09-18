f1b6436c77197bed36f6c57e2a6aae70cf4223b8
moreportiro <mr.k0teikaf@gmail.com>
2025-09-19 00:44:57 +0300

    feat(bot): auth handlers

--- app/handlers.py
+++ app/handlers.py
@@ -31,3 +31,40 @@ async def one(callback: CallbackQuery):
     # уведомление, кнопка не подсвечивается после, show_alert - всплывыющее окно
     await callback.answer('OnEOnE', show_alert=True)
     await callback.message.answer('oneoneoneone')
+
+
+# авторизация
+
+
+class Register(StatesGroup):
+    name = State()
+    age = State()
+    number = State()
+
+
+@router.message(Command('register'))
+async def register(message: Message, state: FSMContext):
+    await state.set_state(Register.name)
+    await message.answer('Введите ваше имя')
+
+
+@router.message(Register.name)
+async def register_name(message: Message, state: FSMContext):
+    await state.update_data(name=message.text)
+    await state.set_state(Register.age)
+    await message.answer('Введите ваш возраст')
+
+
+@router.message(Register.age)
+async def register_age(message: Message, state: FSMContext):
+    await state.update_data(age=message.text)
+    await state.set_state(Register.number)
+    await message.answer('Отправьте номер телефона', reply_markup=kb.get_number)
+
+
+@router.message(Register.number, F.contact)
+async def register_number(message: Message, state: FSMContext):
+    await state.update_data(number=message.contact.phone_number)
+    data = await state.get_data()
+    await message.answer(f'Ваше имя: {data["name"]}\nВаш возраст: {data["age"]}\nНомер: {data["number"]}')
+    await state.clear()
--- app/keyboard.py
+++ app/keyboard.py
@@ -11,3 +11,8 @@ password = InlineKeyboardMarkup(inline_keyboard=[
     [InlineKeyboardButton(text='1', callback_data='1')],
     [InlineKeyboardButton(text='2', callback_data='2')],
     [InlineKeyboardButton(text='3', callback_data='3')],])
+
+get_number = ReplyKeyboardMarkup(keyboard=[
+    [KeyboardButton(text='Отправить номер',
+                    request_contact=True)]],  # запрос контакта
+    resize_keyboard=True)
